1b88b97c27b1b37c0e4c6b922810eec8764afd0c
AddictedToBattlestar <addictedtobsg@gmail.com>
2023-02-27 18:51:21 -0500

    Normalizing the incoming data for the RESTSerializer

--- app/serializers/application.ts
+++ app/serializers/application.ts
@@ -1,3 +1,26 @@
 import RESTSerializer from '@ember-data/serializer/rest';
+import { DS, ModelSchema } from 'ember-data';
+import { isArray } from '@ember/array';
 
-export default class ApplicationSerializer extends RESTSerializer {}
+export default class ApplicationSerializer extends RESTSerializer {
+  normalizeResponse(
+    store: DS.Store,
+    primaryModelClass: ModelSchema,
+    payload: {},
+    id: string | number,
+    requestType: string
+  ): {} {
+    const keyFromModelName: string = isArray(payload)
+      ? `${primaryModelClass.modelName}s`
+      : `${primaryModelClass.modelName}`;
+    let transformedPayload: { [key: string]: any } = {};
+    transformedPayload[keyFromModelName] = payload;
+    return super.normalizeResponse(
+      store,
+      primaryModelClass,
+      transformedPayload,
+      id,
+      requestType
+    );
+  }
+}
