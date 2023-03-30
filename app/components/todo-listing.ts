a1f3dbbf4f95b195b3dc7402ff644dda10474211
AddictedToBattlestar <addictedtobsg@gmail.com>
2023-03-29 21:23:38 -0400

    Added client-side search of the todos

--- app/components/todo-listing.ts
+++ app/components/todo-listing.ts
@@ -13,14 +13,15 @@ export interface Args {
 export default class TodoListingComponent extends Component<Args> {
   @inject('todo') declare todoService: TodoService;
   @inject declare router: RouterService;
-  @tracked
-  selectedTodo: null | TodoModel = null;
+  @tracked selectedTodo: null | TodoModel = null;
+  @tracked filteredTodos: Array<TodoModel> = [];
 
   constructor(owner: unknown, args: Args) {
     super(owner, args);
     this.todoService.selectedTodoMessages$.subscribe((newSelectedTodo) => {
       this.selectedTodo = newSelectedTodo;
     });
+    this.filteredTodos = this.args.todos;
   }
 
   get isNewTodoRouteActive() {
@@ -31,5 +32,18 @@ export default class TodoListingComponent extends Component<Args> {
   transitionToListing() {
     this.todoService.setSelectedTodo(null);
     this.router.transitionTo('todo-listing');
+  }
+
+  @action
+  updateSearch(event: InputEvent) {
+    // @ts-ignore: this works even though TS doesn't like it
+    const searchValue = event.target.value.toLowerCase();
+    if (searchValue === '') {
+      this.filteredTodos = this.args.todos;
+    } else {
+      this.filteredTodos = this.filteredTodos.filter((todo) => {
+        return todo.title.toLowerCase().includes(searchValue);
+      });
+    }
   }
 }
